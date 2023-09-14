e221047364278a4666baf71f81e821d259b953c4
Banson <97927045+bansonsy@users.noreply.github.com>
2023-09-14 16:32:51 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Banson Sy
+// 8/8/23
+// Mini Challenge 3: Asking Questions
+// Tasked with asking the user for inputting their name and time. Then print out the sentence at the end.
+
+// Peer Reviewer: Duc Tran: Program runs good. Everything looks fine and runs. 
+
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("What time did you wake up?");
+string timeUp = Console.ReadLine();
+
+Console.WriteLine(name + " " + "woke up at " + timeUp + " today.");
